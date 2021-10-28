9d95bba9527d3602f2df8b638adbbfae3c6e7f72
Xm798 <48074861+Xm798@users.noreply.github.com>
2021-10-28 13:35:04 +0800

    fix: 优化提醒发送逻辑和标题

--- alert/__init__.py
+++ alert/__init__.py
@@ -9,11 +9,11 @@ from .getinfo.receivedata import receive_data
 
 
 
-def send(status:str,message:str) -> None:
+def send(text:str,status:str,message:str) -> None:
     # message_box = '\n'.join(message)
 
     try:
-        notifiers.send2all(status=status, desp=message)
+        notifiers.send2all(text=text,status=status, desp=message)
     except Exception as e:
         print(e)
 
@@ -27,48 +27,52 @@ def check(uid, cookie, name):
 
     result: str = receive_data(base_data)
     message = "\n".join(result)
+    alert = alert_task = alert_resin =alert_expedition = False
+    status= ""
 
     # 半夜委托没做完时
     if(config.INCOMPLETE_ALERT != "" and base_data.finished_task_num != 4):
         time1 = time.strptime(config.INCOMPLETE_ALERT, '%H%M%S')
         time2 = time.localtime()
         if(time2.tm_hour == time1.tm_hour and time2.tm_min >= time1.tm_min or time2.tm_hour > time1.tm_hour and time2.tm_min < time1.tm_min):
-            send(status="亲爱的亲爱的亲爱的旅行者！你今日的委托还没有完成哦~", message=message)
-            alert_succeed = True
-            log.info('委托检查结束，提醒已发送。')
+            status = "你今日的委托还没有完成哦~"
+            alert_task = True
+            log.info('今日委托未完成，发送提醒。')
         else:
-            alert_succeed = False
+            alert_task = False
             log.info('今日委托未完成，未到提醒时间。')
     else:
-        alert_succeed = False
+        alert_task = False
         log.info('委托检查结束，今日委托已完成。（或未配置委托未完成提醒）')
 
     # 树脂达到临界时
-    if (not alert_succeed):
-        if(base_data.current_resin >= int(config.RESIN_ALERT_NUM)):
-            if(base_data.current_resin >= 160):
-                send(status="亲爱的亲爱的亲爱的旅行者！树脂已经溢出啦！", message=message)
-            else:
-                send(status="亲爱的亲爱的亲爱的旅行者！树脂快要溢出啦！", message=message)
-            alert_succeed = True
-            log.info('树脂已到临界值，提醒已发送。')
+    if(base_data.current_resin >= int(config.RESIN_ALERT_NUM)):
+        if(base_data.current_resin >= 160):
+            status= status + "树脂已经溢出啦！"
         else:
-            alert_succeed = False
-            log.info('树脂检查结束，未到提醒临界值。')
+            status= status + "树脂快要溢出啦！"
+        alert_resin = True
+        log.info('树脂已到临界值，发送提醒。')
+    else:
+        alert_resin = False
+        log.info('树脂检查结束，未到提醒临界值。')
 
-    if (not alert_succeed):
-        if(config.EXPEDITION_COMPLETE_ALERT):
-            if("已完成" in message):
-                send(status="亲爱的亲爱的亲爱的旅行者！探索派遣已经完成啦！", message=message)
-                alert_succeed = True
-                log.info('探索派遣已完成，提醒已发送。')
-            else:
-                alert_succeed = False
-                log.info('探索派遣检查结束，不存在完成的探索派遣。')
+    if(config.EXPEDITION_COMPLETE_ALERT == "ON"):
+        if("已完成" in message):
+            status= status + "探索派遣已经完成啦！"
+            alert_expedition = True
+            log.info('探索派遣已完成，发送提醒。')
         else:
-            log.info('不提醒探索派遣完成情况，跳过探索派遣检查。')
+            alert_expedition = False
+            log.info('探索派遣检查结束，不存在完成的探索派遣。')
+    else:
+        log.info('不提醒探索派遣完成情况，跳过探索派遣检查。')
 
-    return alert_succeed
+    alert = alert_resin or alert_task or alert_expedition
+    if alert:
+        send(text="亲爱的亲爱的亲爱的旅行者！",status=status, message=message)
+
+    return alert
 
 def main() -> None:
     uid = config.UID
@@ -76,9 +80,9 @@ def main() -> None:
     name = config.NAME
 
     while True:
-        alert_succeed = check(uid, cookie, name)
+        alert= check(uid, cookie, name)
 
-        if alert_succeed :
+        if alert :
             log.info(f'本轮运行结束，提醒已发送，休眠{config.ALERT_SUCCESS_SLEEP_TIME}秒')
             time.sleep(config.ALERT_SUCCESS_SLEEP_TIME)
         else:
