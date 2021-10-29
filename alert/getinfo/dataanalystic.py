23d47c97c5412d93c0799d601d587cbb7a4dfb66
student_2333 <lgc2333@126.com>
2021-10-30 00:34:14 +0800

    feat: 加入每日委托奖励是否领取、下个树脂回复时间、已完成探索派遣数量

--- alert/getinfo/dataanalystic.py
+++ alert/getinfo/dataanalystic.py
@@ -13,18 +13,19 @@ def get_resin_data(base_data: BaseData) -> str:
     resin_data = f"当前树脂：{current_resin}\n"
     if(base_data.current_resin != 160):
         resin_recovery_time = seconds2hours(base_data.resin_recovery_time)
-        resin_data += f"树脂恢复时间：{resin_recovery_time}"
+        next_resin_rec_time = seconds2hours(
+            8 * 60 - ((base_data.max_resin - base_data.current_resin) * 8 * 60 - base_data.resin_recovery_time))
+        resin_data += f"下个/全部树脂恢复时间：{next_resin_rec_time}/{resin_recovery_time}"
     return resin_data
 
 
 def get_resin_discount_data(base_data: BaseData) -> str:
-    count: int = base_data.resin_discount_num_limit
-    return f"本周boss战树脂减半剩余使用次数：{3-count}/3"
+    return f"本周boss战树脂减半剩余使用次数：{base_data.remain_resin_discount_num}/{base_data.resin_discount_num_limit}"
 
 
 def get_task_num_data(base_data: BaseData) -> str:
     task_num: str = f"{base_data.finished_task_num}/{base_data.total_task_num}"
-    return f"今日完成委托数量：{task_num}"
+    return f"今日完成委托数量：{task_num} 奖励{'已' if base_data.is_extra_task_reward_received else '未'}领取"
 
 
 def get_expedition_data(base_data: BaseData) -> str:
@@ -33,8 +34,8 @@ def get_expedition_data(base_data: BaseData) -> str:
     with open(config_file, 'r', encoding='utf-8') as f:
         avatar_json = json.load(f)
 
-    expedition_num: str = f"{base_data.current_expedition_num}/{base_data.max_expedition_num}"
     expedition_info: list[str] = []
+    finished = 0
     for expedition in base_data.expeditions:
         avatar: str = expedition['avatar_side_icon'][89:-4]
         try:
@@ -44,11 +45,12 @@ def get_expedition_data(base_data: BaseData) -> str:
 
         if(expedition['status'] == 'Finished'):
             expedition_info.append(f"{avatar_name} 已完成")
+            finished += 1
         else:
-            remained_timed: str = seconds2hours(
-                expedition['remained_time'])
+            remained_timed: str = seconds2hours(expedition['remained_time'])
             expedition_info.append(
                 f"{avatar_name} 未完成，剩余时间{remained_timed}")
 
+    expedition_num: str = f"{base_data.current_expedition_num}/{finished}/{base_data.max_expedition_num}"
     expedition_data: str = "\n".join(expedition_info)
-    return f"当前探索派遣数量：{expedition_num}\n详细信息:\n{expedition_data}"
+    return f"当前探索派遣总数/完成/上限：{expedition_num}\n详细信息:\n{expedition_data}"
