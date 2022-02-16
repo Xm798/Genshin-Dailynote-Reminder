c1ec5752dfb2cd8d6cd000bbb90d78a90a94dd54
Xm798 <48074861+Xm798@users.noreply.github.com>
2022-02-16 22:15:10 +0800

    🔧 build(serverless): Aliyun FC support

--- serverless.py
+++ serverless.py
@@ -1,7 +1,14 @@
 from dailynotehelper.__banner__ import banner
 from dailynotehelper import run_once
 
+# Tencent SCF
 def main_handler(event, context):
+    print(banner)
+    run_once()
+    return
+
+# Aliyun FC
+def handler(event, context):
     print(banner)
     run_once()
     return
