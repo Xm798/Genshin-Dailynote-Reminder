a5259f1446617828a2d1726fe587999ed6692508
Xm798 <git@xm.mk>
2022-11-15 20:19:16 +0800

    🐞 fix: crash when locale to en_US

--- dailynotehelper/__banner__.py
+++ dailynotehelper/__banner__.py
@@ -3,8 +3,8 @@ banner = f'''
 -------------------------------------------------------------
 |                Genshin Dailynote Helper                   |
 -------------------------------------------------------------
-| Current Version: v2.1.14                                  |
-| Last Update: Nov 06, 2022                                 |
+| Current Version: v2.1.15                                  |
+| Last Update: Nov 15, 2022                                 |
 | Github: https://github.com/Xm798/Genshin-Dailynote-Helper |
 -------------------------------------------------------------
 '''
