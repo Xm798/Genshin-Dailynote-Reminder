680a26c5767009a963bbbaa29fbbc0db69983c7d
Xm798 <git@xm.mk>
2022-11-17 00:55:47 +0800

    📃 docs: v2.2.0

--- dailynotehelper/__banner__.py
+++ dailynotehelper/__banner__.py
@@ -3,8 +3,8 @@ banner = f'''
 -------------------------------------------------------------
 |                Genshin Dailynote Helper                   |
 -------------------------------------------------------------
-| Current Version: v2.1.15                                  |
-| Last Update: Nov 15, 2022                                 |
+| Current Version: v2.2.0                                   |
+| Last Update: Nov 17, 2022                                 |
 | Github: https://github.com/Xm798/Genshin-Dailynote-Helper |
 -------------------------------------------------------------
 '''
